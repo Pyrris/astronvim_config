eb1b3636fb6e5c0564fb12f315ba9a548384325f
pyrristitan@gmail.com <pyrristitan@gmail.com>
2025-03-12 09:22:12 +0800

    Added catppuccin theme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,19 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  opts = {
+    transparent_background = true,
+    background = {
+      light = "latte",
+      dark = "mocha",
+    },
+    dim_inactive = {
+      enabled = false,
+      shade = "dark",
+      percentage = 0.20,
+    },
+    integrations = {
+      notify = true,
+    },
+  },
+}
